b2204c300956596ea04e0691aec269ce74c4bfd2
Marc Brinkmann <git@marcbrinkmann.de>
2020-10-25 01:09:16 +0200

    Finish remainder of docs

--- src/tpl/elements.rs
+++ src/tpl/elements.rs
@@ -1,5 +1,13 @@
+//! Predefined LaTeX elements.
+//!
+//! Contains a number of elements that can be used to generate LaTeX code. See the `tpl` module
+//! documentation for an example and comparison.
+
 use super::{Args, BeginEndBlock, Group, MacroCall, OptArgs, RawTex, TexElement, Text};
 
+/// A no-item iterator.
+///
+/// Can be passed to `Args::new` or `OptArgs::new` to indicate no arguments.
 #[derive(Copy, Clone, Debug)]
 pub struct Nothing;
 
@@ -12,16 +20,20 @@ impl Iterator for Nothing {
     }
 }
 
+/// Ready to use instance of `Nothing`.
 pub const N: Nothing = Nothing;
 
+/// Creates a new text element.
 pub fn t<S: Into<String>>(s: S) -> Text {
     Text::new(s)
 }
 
+/// Creates a new top-level document.
 pub fn doc(children: Vec<Box<dyn TexElement>>) -> Group {
     Group(children)
 }
 
+/// Creates a `documentclass` declaration.
 pub fn documentclass<S: AsRef<str>, I: IntoIterator<Item = S>>(
     opt_args: I,
     doc_class: &str,
@@ -33,6 +45,7 @@ pub fn documentclass<S: AsRef<str>, I: IntoIterator<Item = S>>(
     )
 }
 
+/// Creates a `usepackage` declaration.
 pub fn usepackage<S: Into<String>>(
     opt_args: Vec<Box<dyn TexElement>>,
     package_name: S,
@@ -47,10 +60,12 @@ pub fn usepackage<S: Into<String>>(
     }
 }
 
+/// Creates a new `document` begin/end block.
 pub fn document(children: Vec<Box<dyn TexElement>>) -> BeginEndBlock {
     BeginEndBlock::new("document", OptArgs::default(), Args::default(), children)
 }
 
+/// Creates a new `section` header.
 pub fn section<S: Into<String>>(title: S) -> MacroCall {
     MacroCall {
         ident: RawTex::new("section"),
@@ -60,6 +75,7 @@ pub fn section<S: Into<String>>(title: S) -> MacroCall {
     }
 }
 
+/// Creates a new `subsection` header.
 pub fn subsection<S: Into<String>>(title: S) -> MacroCall {
     MacroCall {
         ident: RawTex::new("subsection"),
